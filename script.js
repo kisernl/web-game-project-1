0dd52eb0d403711c9e6e0251b5dc7fa39c6efe08
Noah Kiser <noah.kiser@gmail.com>
2024-10-03 19:58:04 -0500

    adjust top position and animation speed

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const spriteHeight = 523;
 // let frameX = 0;
 // let frameY = 0;
 let gameFrame = 0;
-const staggerFrames = 5;
+const staggerFrames = 4;
 // added for use with advanced animation
 const spriteAnimations = [];
 const animationStates = [
